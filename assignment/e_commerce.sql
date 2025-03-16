8b8a3b3bd22abe68853f960cc3058230f4eea46c
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-16 13:47:58 +0100

    added the foreign key

--- assignment/e_commerce.sql
+++ assignment/e_commerce.sql
@@ -18,10 +18,17 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `orders` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `product_id` int NOT NULL,
-  `quantity` decimal(10,0) NOT NULL,
-  `unit` enum('ctn','dz','pkt','roll','pcs') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    `no` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `product_id` INT NOT NULL,
+    `quantity` DECIMAL(10 , 0 ) NOT NULL,
+    `unit` ENUM('ctn', 'dz', 'pkt', 'roll', 'pcs') NOT NULL,
+    PRIMARY KEY (`no`),
+    INDEX (user_id),
+    INDEX (product_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (product_id)
+        REFERENCES products (id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_0900_AI_CI;

90572fa786b8a3c7f7849908608464cfb62f03b3
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-19 08:54:12 +0100

    Completed exercise 2.20 - 2.25

--- assignment/chapter_2/chapter2_answers.sql
+++ assignment/chapter_2/chapter2_answers.sql
@@ -9,3 +9,26 @@ SELECT SKU_Description, SKU FROM INVENTORY;
 
 /* *** Questions 2.19 *** */
 SELECT WarehouseID FROM INVENTORY;
+
+/* *** Questions 2.20 *** */
+SELECT DISTINCT WarehouseID FROM INVENTORY;
+
+/* *** Questions 2.21 *** */
+SELECT WarehouseID, SKU, SKU_Description, QuantityOnHand, QuantityOnOrder FROM INVENTORY;
+
+/* *** Questions 2.22 *** */
+SELECT * FROM INVENTORY;
+
+/* *** Questions 2.23 *** */
+SELECT * FROM INVENTORY WHERE QuantityOnHand > 0;
+
+/* *** Questions 2.24 *** */
+SELECT * FROM INVENTORY WHERE QuantityOnHand = 0;
+
+/* *** Questions 2.25 *** */
+SELECT SKU, SKU_Description, WarehouseID FROM INVENTORY WHERE QuantityOnHand = 0 ORDER BY WarehouseID;
+
+
+
+
+

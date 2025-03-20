c06155a2ef32f8fdcecedbabd476bfa95c45948d
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-03-20 08:46:49 +0100

    latest changes

--- chapter_2/assignment/chapter2_answers.sql
+++ chapter_2/assignment/chapter2_answers.sql
@@ -31,8 +31,42 @@ SELECT SKU, SKU_Description, WarehouseID FROM INVENTORY WHERE QuantityOnHand = 0
 SELECT SKU, SKU_Description, WarehouseID FROM INVENTORY WHERE QuantityOnHand > 0 ORDER BY WarehouseID DESC, SKU;
 
 /* *** Questions 2.27 *** */
+SELECT SKU, SKU_Description, WarehouseID FROM INVENTORY WHERE QuantityOnHand = 0 AND QuantityOnOrder > 0 
+ORDER BY WarehouseID DESC , SKU;
 
+/* *** Questions 2.28 *** */
+SELECT SKU, SKU_Description, WarehouseID FROM INVENTORY where QuantityOnHand = 0 or QuantityOnOrder = 0 
+ORDER BY WarehouseID DESC, SKU;
 
+/* *** Questions 2.29 *** */
+SELECT SKU, SKU_Description, WarehouseID, QuantityOnHand FROM INVENTORY WHERE QuantityOnHand > 1 AND QuantityOnHand < 10;
 
+/* *** Questions 2.30 *** */
+SELECT SKU, SKU_Description, WarehouseID, QuantityOnHand FROM INVENTORY WHERE QuantityOnHand BETWEEN 2 AND 9;
 
+/* *** Questions 2.31 *** */
+SELECT distinct SKU, SKU_Description FROM INVENTORY WHERE SKU_Description LIKE "Half-Dome%";
+
+/* *** Questions 2.32 *** */
+SELECT distinct SKU, SKU_Description FROM INVENTORY WHERE SKU_Description LIKE "%Climb%";
+
+/* *** Questions 2.33 *** */
+SELECT distinct SKU, SKU_Description FROM INVENTORY WHERE SKU_Description LIKE "__d%";
+
+/* *** Questions 2.34 *** */
+SELECT distinct SKU, SKU_Description FROM INVENTORY WHERE SKU_Description LIKE "__d%";
+
+/* *** Questions 2.34 *** */
+SELECT COUNT(QuantityOnHand) AS NumberOfRows  FROM INVENTORY;
+SELECT SUM(QuantityOnHand) AS SumOfQuantityOnHand  FROM INVENTORY;
+SELECT AVG(QuantityOnHand) AS AvgQuantityOnHand  FROM INVENTORY;
+SELECT MIN(QuantityOnHand) AS MinQuantityOnHand  FROM INVENTORY;
+SELECT MAX(QuantityOnHand) AS MaxQuantityOnHand  FROM INVENTORY;
+
+/* *** Questions 2.35 *** */
+/*Explain the difference between the SQL built-in functions COUNT and SUM.
+	COUNT: this is used to count the number of rows in column, a roll is counted if it has a non-null value
+	
+    
+*/
 

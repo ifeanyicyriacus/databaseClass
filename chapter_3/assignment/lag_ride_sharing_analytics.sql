73ab7c0e5da11f0cb58cac04096ef8801bf60a51
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-04-16 11:39:24 +0100

    Completed exercise_1-4

--- chapter_3/assignment/lag_ride_sharing_analytics.sql
+++ chapter_3/assignment/lag_ride_sharing_analytics.sql
@@ -1,33 +1,34 @@
 create database lag_ride_sharing_analytics;
+
 use lag_ride_sharing_analytics;
 
-CREATE TABLE `drivers` (
+CREATE TABLE `Drivers` (
   `DriverID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) NOT NULL,
-  `Rating` decimal(1,1) NOT NULL,
+  `Rating` decimal(2,1) NOT NULL,
   `TotalRides` int NOT NULL,
   PRIMARY KEY (`DriverID`)
 );
 
-CREATE TABLE `riders` (
+CREATE TABLE `Riders` (
 	`RiderID` int NOT NULL AUTO_INCREMENT,
     `Name` varchar(45) NOT NULL,
     `City` varchar(45) NOT NULL,
     PRIMARY KEY (`RiderID`)
 );
 
-CREATE TABLE `rides` (
+CREATE TABLE `Rides` (
 	`RideID` int NOT NULL AUTO_INCREMENT ,
     `DriverID` int NOT NULL,
     `RiderID` int NOT NULL,
     `Fare` int NOT NULL,
-    `Rating` decimal(1,1) NOT NULL,
+    `Rating` decimal(2,1) NOT NULL,
     `DistanceKM` int NOT NULL,
     PRIMARY KEY (`RideID`),
     INDEX `driver_ind` (DriverID),
     INDEX `rider_ind` (RiderID),
-    FOREIGN KEY (DriverID) REFERENCES drivers(DriverID),
-	FOREIGN KEY (RiderID) REFERENCES riders(RiderID)
+    FOREIGN KEY (DriverID) REFERENCES Drivers(DriverID),
+	FOREIGN KEY (RiderID) REFERENCES Riders(RiderID)
 );
 
 CREATE TABLE `Payments` (
@@ -37,6 +38,6 @@ CREATE TABLE `Payments` (
     `PaymentMethod` varchar(45) NOT NULL,
     PRIMARY KEY (`PaymentID`),
     INDEX `ride_ind` (RideID),
-    FOREIGN KEY (RideID) REFERENCES rides(RideID)
+    FOREIGN KEY (RideID) REFERENCES Rides(RideID)
 );
 

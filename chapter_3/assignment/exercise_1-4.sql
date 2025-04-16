63fde52d62ec1a1065e15e1d23a9aaa9595ed0e0
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-04-16 20:36:17 +0100

    edited exercise_1-4

--- chapter_3/assignment/exercise_1-4.sql
+++ chapter_3/assignment/exercise_1-4.sql
@@ -1,23 +1,23 @@
 use lag_ride_sharing_analytics;
 
 -- 1. Top 5 highest-rated drivers in Lagos
-select * from Drivers
+create table top_5rated_driver as (select * from Drivers
 order by Rating desc, TotalRides desc
-limit 5;
+limit 5);
 
 -- 2. Riders with more than 5 rides in the last 30 days in Lagos
-select Riders.RiderID, Riders.Name, City, count(*) as rides  
+create table riders_with_more_than_5_rides as (select Riders.RiderID, Riders.Name, City, count(*) as rides  
 from Riders join Rides
 on Riders.RiderID = Rides.RiderID
 group by Riders.RiderID
-having rides > 5;
+having rides > 5);
 
 -- 3. Total revenue for the past month for Lagos rides
-select sum(Amount) as TotalRevenue from Payments;
+create table total_revenue_for_past_month as (select sum(Amount) as TotalRevenue from Payments);
 
 -- 4. Area in Lagos with the most rides
-select City, count(*) as Rides from Riders join Rides
+create table area_in_lagos_with_most_rides as (select City, count(*) as Rides from Riders join Rides
 on Riders.RiderID = Rides.RiderID
 group by City
-order by Rides desc;
+order by Rides desc);
 
